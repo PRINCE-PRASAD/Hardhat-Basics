98b5d7350d520c1f7fd162ec54e5d363b7b77c3e
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-03-22 15:07:22 +0530

    test2

--- Part1/test/token.js
+++ Part1/test/token.js
@@ -6,14 +6,33 @@ describe("Token Contract", function () {
 
         // A Signer in Ethers.js is an object that represents an Ethereum account. It's used to send transactions to contracts and other accounts. Here we're getting a list of the accounts in the node we're connected to, which in this case is Hardhat Network, and only keeping the first and second ones.
 
-        console.log("Singer object:", owner);
+        // console.log("Singer object:", owner);
         const Token = await ethers.getContractFactory("Token");
 // A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts, so Greeter here is a factory for instances of our greeter contract.
         const hardhatToken = await Token.deploy();
 
         const ownerBalance = await hardhatToken.balanceof(owner.address);
-        console.log("Owner Address:", owner.address);
+        // console.log("Owner Address:", owner.address);
 
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
-});
+
+    it("Should transfer token between accounts", async function (){
+        const [owner, addr1, addr2] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("Token");
+
+        const hardhatToken = await Token.deploy();
+
+        // transfer 10 token from owner to addr1
+
+        await hardhatToken.transfer(addr1.address, 10);
+        expect(await hardhatToken.balanceof(addr1.address)).to.equal(10);
+
+        // Transfer 5 token from addr1 to addr2  
+
+        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+        expect(await hardhatToken.balanceof(addr2.address)).to.equal(5);
+
+    });
+}); 
